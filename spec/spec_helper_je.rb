ae16076a2c8ad66d4d22b93168a6846b05f63a7d
James Eckersall <james.eckersall@fasthosts.com>
2016-06-17 10:21:04 +0100

    new spec_helper_je.rb file

---
+++ spec/spec_helper_je.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require 'serverspec'
+require 'docker'
+require_relative '../../drone-tests/shared/jemonkeypatch.rb'
+
+MYSQL_PORT = 3306
+LISTEN_PORT = 8080
+
+base_spec_dir = Pathname.new(File.join(File.dirname(__FILE__)))
+Dir[base_spec_dir.join('../../drone-tests/shared/**/*.rb')].sort.each { |f| require_relative f }
+
+set :backend, :docker
+@image = Docker::Image.get(ENV['IMAGE'])
+set :docker_image, @image.id
+#set :docker_debug, true
+set :docker_container_start_timeout, 60
+set :docker_container_ready_regex, /mysqld_safe Starting mysqld daemon with databases from \/var\/lib\/mysql/
+
+set :docker_container_create_options, {
+  'Image'      => @image.id,
+  'User'       => '100000',
+  'HostConfig' => {
+    'PortBindings' => { "#{MYSQL_PORT}/tcp" => [{ 'HostPort' => "#{MYSQL_PORT}" }] }
+  }
+}
+
+RSpec.configure do |c|
+
+  describe 'tests' do
+    describe package('mysql-server') do
+      it { should be_installed }
+    end
+
+    include_examples 'docker-ubuntu-16'
+    include_examples 'docker-ubuntu-16-nginx-1.10.0'
+    include_examples 'php-7.0-tests'
+    include_examples 'phpmyadmin-4.6-tests'
+    include_examples 'mysql-tests'
+  end
+
+end
